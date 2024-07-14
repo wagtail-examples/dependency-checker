7659f5759b63f2615f381260e7ff326f8e0a768e
nickmoreton <nick.moreton@torchbox.com>
2024-07-14 22:36:00 +0100

    Just a reformat

--- src/main.py
+++ src/main.py
@@ -4,45 +4,26 @@ from src.check_remote import check_remote
 
 
 @click.group()
-def cli():
+@click.version_option(version="0.1.0", prog_name="Dependency Checker")
+@click.option("--report", "-r", is_flag=True, help="Generate a printable report.")
+@click.pass_context
+def cli(ctx: click.Context, report: bool):
     """Dependency Checker CLI tool."""
+    ctx.ensure_object(dict)
+    ctx.obj["report"] = report
 
 
 @cli.command()
-@click.option(
-    "--repo-url",
-    prompt="Repository URL",
-    help="The URL of the repository to clone.",
-)
-@click.option(
-    "--report",
-    "-r",
-    is_flag=True,
-    help="Generate a printable report.",
-)
-def remote(repo_url, report):
-    """
-    Analyze the dependencies of a remote repository.
-    """
-
-    check_remote(repo_url, report)
+@click.option("--repo-url", prompt="Repository URL", help="The URL of the repository to clone.")
+@click.pass_context
+def remote(ctx: click.Context, repo_url: str):
+    """Analyze the dependencies of a remote repository"""
+    check_remote(repo_url, ctx.obj["report"])
 
 
 @cli.command()
-@click.option(
-    "--path",
-    prompt="Path to the repository",
-    help="The path to the repository to check.",
-)
-@click.option(
-    "--report",
-    "-r",
-    is_flag=True,
-    help="Generate a printable report.",
-)
-def local(path, report):
-    """
-    Analyze the dependencies of a local repository.
-    """
-
-    check_local(path, report)
+@click.option("--path", prompt="Path to the repository", help="The path to the repository to check.")
+@click.pass_context
+def local(ctx: click.Context, path: str):
+    """Analyze the dependencies of a local repository"""
+    check_local(path, ctx.obj["report"])
